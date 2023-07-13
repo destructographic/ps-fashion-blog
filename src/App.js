dd6cfceee5b623fa42938a8f36b29a8db8fbb107
justin@archbox <driftpattern@gmail.com>
2023-07-13 02:00:50 -0700

    additional styling change. assignment complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 
 
 import './App.css';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Article from './components/Article';
 import Footer from './components/Footer';
 
 
 function App() {
+  useEffect(() => {
+    document.title = 'R-ALAB 320H.1.2 - React Fashion Blog';
+  }, []);
+  
   return (
     <div className="App">
       <Header />
