f9db7d128a2bb479897ba00d97176d5de47fbdbe
justin@archbox <driftpattern@gmail.com>
2023-07-13 01:41:11 -0700

    added footer. multiple css corrections.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@
 import './App.css';
 import Header from './components/Header';
 import Article from './components/Article';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Article />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,2 +1,25 @@
 // components/Footer/index.js
 
+import React from 'react';
+import './index.css';
+
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li><a href="#">Women’s</a></li>
+          <li><a href="#">Men’s</a></li>
+          <li><a href="#">On the Street</a></li>
+          <li><a href="#">The Catwalk</a></li>
+          <li><a href="#">AdWatch</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+      <p>&copy; {new Date().getFullYear()} Your Fashion Blog. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
